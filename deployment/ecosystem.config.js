387458db10fdc334ac4fb7b52176b5f21b19ac6c
hamzathaneesmib <hamzath.anees@mib.com.mv>
2025-12-13 08:29:44 +0500

    fully static now 4

--- deployment/ecosystem.config.js
+++ deployment/ecosystem.config.js
@@ -1,19 +1,20 @@
 module.exports = {
-  apps: [{
-    name: 'thetimelesstours',
-    script: 'npx',
-    args: 'serve -s dist -l 3000',
-    instances: 1,
-    autorestart: true,
-    watch: false,
-    max_memory_restart: '1G',
-    env: {
-      NODE_ENV: 'production',
-      PORT: 3000
-    },
-    error_file: '/var/log/pm2/thetimelesstours-error.log',
-    out_file: '/var/log/pm2/thetimelesstours-out.log',
-    log_date_format: 'YYYY-MM-DD HH:mm:ss Z'
-  }]
+	apps: [
+		{
+			name: 'thetimelesstours',
+			script: 'npx',
+			args: 'serve -s dist -l 3000',
+			instances: 1,
+			autorestart: true,
+			watch: false,
+			max_memory_restart: '1G',
+			env: {
+				NODE_ENV: 'production',
+				PORT: 3000,
+			},
+			error_file: '/var/log/pm2/thetimelesstours-error.log',
+			out_file: '/var/log/pm2/thetimelesstours-out.log',
+			log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
+		},
+	],
 };
-
